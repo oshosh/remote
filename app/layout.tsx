4be705c4bb9f618b79462ce0d920d772120ea63c
OSH <42954693+oshosh@users.noreply.github.com>
2025-09-10 14:07:13 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Remote App',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
